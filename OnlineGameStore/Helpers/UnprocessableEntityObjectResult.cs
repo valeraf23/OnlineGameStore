f64cdc966a156829443e8a3c5cbf936eb42ac25b
Valeraf23 <valeraf23@gmail.com>
2019-11-27 16:58:46 +0200

    remove using

--- OnlineGameStore/Helpers/UnprocessableEntityObjectResult.cs
+++ OnlineGameStore/Helpers/UnprocessableEntityObjectResult.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using OnlineGameStore.Common.Errors;
 
 namespace OnlineGameStore.Api.Helpers
 {
