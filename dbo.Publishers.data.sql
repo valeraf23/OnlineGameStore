a2c02e0e75108b231b79c9d529a5736b08963cf4
Valerii Fedorenko <valeraf23@gmail.com>
2019-11-27 16:56:18 +0200

    delete sql3

--- dbo.Publishers.data.sql
+++
@@ -1,6 +0,0 @@
-﻿INSERT INTO [dbo].[Publishers] ([Id], [Name]) VALUES (N'e99d98ab-56d2-400a-93eb-65d731d9144d', N'FirstUs')
-INSERT INTO [dbo].[Publishers] ([Id], [Name]) VALUES (N'5213789e-90f9-4098-aa5a-95e9cd69bf1c', N'FirstUser_UpdatedVF_Second')
-INSERT INTO [dbo].[Publishers] ([Id], [Name]) VALUES (N'3fc85173-2888-4355-8b73-ae6cdc7f8568', N'FirstUser_UpdatedVF_1')
-INSERT INTO [dbo].[Publishers] ([Id], [Name]) VALUES (N'73e3b486-ffb9-4de5-aa92-c15fc6843ca2', N'FirstU3s')
-INSERT INTO [dbo].[Publishers] ([Id], [Name]) VALUES (N'73e3b486-ffb9-4de5-aa92-c15fc6843ca2', N'FirstU3s')
-
